198f3e4d2fa9d1d088f03a674dcf9a985f3f7772
ograndoptimist <gabrieliprj@gmail.com>
2019-06-26 18:45:03 -0300

    main executing of the program

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from crawler import CrawlerTree
+import random
 
 
 def main(url_beggining="https://pt.wikipedia.org/wiki/Brasilac",
          ):
     documentsTree = CrawlerTree()
-    print(documentsTree.insert_node(url=url_beggining))
+
+    items = []
+    for k in range(100):
+        items.append(random.randint(0, 500))
+
+    for item in items:
+        print("item a se adicionar: ", item)
+        documentsTree.insert_node(url=item)
+
+    print("Nós inseridos: {0}".format(documentsTree.number_of_nodes()))
     
 
-
-
-
 if __name__ == '__main__':
-      main()
+    main()
