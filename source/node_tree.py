8712746dd3a03546ec185bacf8600f5f1a56a470
ograndoptimist <gabrieliprj@gmail.com>
2019-06-27 17:52:03 -0300

    node that acts as a baseline model to build the crawler tree

--- source/node_tree.py
+++ source/node_tree.py
@@ -3,8 +3,8 @@ class NodeTree(object):
        Abstract data type that provides the baseline to build a Tree Crawler.
     """
     def __init__(self, item=None):
-        self.child_left = None
-        self.child_right = None
+        self.left = None
+        self.right = None
         self.item = item
         self.content = None
         self.hash = None
